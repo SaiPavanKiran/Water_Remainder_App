b933d74cab246a81953dfc1b6258266c54dc0dcd
SaiPavanKiran <saipavankiran9@gmail.com>
2024-09-02 15:41:47 +0530

    Update Theme.kt

--- app/src/main/java/com/rspk/water_remainder_app/ui/theme/Theme.kt
+++ app/src/main/java/com/rspk/water_remainder_app/ui/theme/Theme.kt
@@ -260,10 +260,10 @@ fun Water_Remainder_AppTheme(
     content: @Composable() () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+        // dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        //     val context = LocalContext.current
+        //     if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        // }
 
         darkTheme -> darkScheme
         else -> lightScheme
